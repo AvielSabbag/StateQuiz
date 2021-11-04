6df1b86ad81ab199e4c1eee477f0e8a8b7954c59
kevanjk <kevankad@gmail.com>
2021-11-04 16:39:42 -0400

    New files added

---
+++ app/src/main/java/edu/uga/cs/statequiz/CapitalQuizQuestion.java
@@ -0,0 +1,76 @@
+package edu.uga.cs.statequiz;
+/** POJO class for quiz quesiton */
+public class CapitalQuizQuestion {
+
+    private int id;
+    private String state;
+    private String capital;
+    private String city;
+    private String city1;
+    private String answer;
+
+    public CapitalQuizQuestion(){
+        this.id = -1;
+        this.state = "/";
+        this.capital = "/";
+        this.city = "/";
+        this.city1 = "/";
+        this.answer = "/";
+    }
+
+    public CapitalQuizQuestion(String state, String capital, String city, String city1){
+        this.id = -1;
+        this.state = state;
+        this.capital = capital;
+        this.city = city;
+        this.city1 = city1;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+    public String getCity2() {
+        return city;
+    }
+
+    public void setCity2(String city2) {
+        this.city = city;
+    }
+
+    public String getCity3() {
+        return city1;
+    }
+
+    public void setCity3(String city3) {
+        this.city1 = city1;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+}
